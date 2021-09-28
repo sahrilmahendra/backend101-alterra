da5ce9b575b21cbffe2e7fb6ec456325ea2f38fb
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-09-28 20:20:11 +0700

    menambahkan perhitungan segittiga

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// angkaku := 10
+
+	// fmt.Println(angkaku)
+
+	// fmt.Println("Hello World")
+
+	alas := 10
+	tinggi := 15
+	L := (alas * tinggi) / 2
+	fmt.Println("Luas segitiga =", L)
+
+	helloWorld := "hello" + " " + "world"
+	fmt.Println(helloWorld)
+
 }
